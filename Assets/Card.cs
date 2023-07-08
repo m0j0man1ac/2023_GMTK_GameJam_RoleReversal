0d8c3dc9265212cb037c383453c9a4aa618f2911
m0j0man1ac <113955916+m0j0man1ac@users.noreply.github.com>
2023-07-08 16:03:56 +1000

    Card object

---
+++ Assets/Card.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Card : ScriptableObject
+{
+    public new string name;
+    public Sprite artIcon;
+    public string desription;
+    public int energyCost;
+
+    //public string cardType;
+
+    public float dramaVal;
+    public float braveryVal;
+
+    public float damage;
+    public float healing;
+}
