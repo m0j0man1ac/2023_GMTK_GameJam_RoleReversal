431c9f3e91e9309c7914919ee36f7ab36db128b2
m0j0man1ac <113955916+m0j0man1ac@users.noreply.github.com>
2023-07-09 03:54:20 +1000

    start of card system

---
+++ Assets/CardDisplay.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card setCard;
+
+    public TMP_Text cardName;
+    public TMP_Text description;
+    public SpriteRenderer spriteR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(setCard!=null)
+            CardUpdate(setCard);
+    }
+
+    public void CardUpdate(Card card)
+    {
+        cardName.text = card.name;
+        description.text = card.desription;
+        spriteR.sprite = card.artIcon;
+    }
+}
